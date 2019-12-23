012afa476912d96ae8f11ccbc5955d1eca25b278
roshanrc99 <56046078+roshanrc99@users.noreply.github.com>
2019-12-23 17:01:01 +0530

    Create Maxduplicate.java

---
+++ Maxduplicate.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+    int i,j,n,max=0;
+    n=sc.nextInt();
+    int a[]=new int[n];
+    int s[]=new int[100];
+    for(i=0;i<n;i++)
+    {
+       a[i]=sc.nextInt();
+    }
+ 
+   for (i=0;i<n;i++)
+   { 
+    s[a[i]]++;
+    }
+    for(i=0;i<100;i++)
+    {
+    if(s[i]>max)
+    {
+        max=a[i];
+    }
+   }
+ System.out.println(a[max]);
+}
+}
